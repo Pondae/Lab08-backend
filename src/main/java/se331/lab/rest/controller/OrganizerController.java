c54e07afa50216372cac503b4733dcfd1502791a
Domdae <domdae@Phonmongkons-MacBook-Pro.local>
2021-09-02 23:21:13 +0700

    re:(7.1,7.2)chk pt5,6

--- src/main/java/se331/lab/rest/controller/OrganizerController.java
+++ src/main/java/se331/lab/rest/controller/OrganizerController.java
@@ -5,19 +5,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import se331.lab.rest.entity.Event;
-import se331.lab.rest.service.EventService;
+import se331.lab.rest.entity.Organizer;
+import se331.lab.rest.service.OrganizerService;
 
+@Controller
 public class OrganizerController {
     @Autowired
-    EventService eventService;
+    OrganizerService organizerService;
 
     @GetMapping("organizers")
-    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage
+    public ResponseEntity<?> getOrganizerLists(@RequestParam(value = "_limit", required = false) Integer perPage
             , @RequestParam(value = "_page", required = false) Integer page) {
-        Page<Event> pageOutput = eventService.getEvents(perPage, page);
+        Page<Organizer> pageOutput = organizerService.getOrganizers(perPage, page);
         HttpHeaders responseHeader = new HttpHeaders();
         responseHeader.set("x-total-count",
                 String.valueOf(pageOutput.getTotalElements()));
@@ -25,9 +27,9 @@ public class OrganizerController {
     }
 
     @GetMapping("organizers/{id}")
-    public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
+    public ResponseEntity<?> getOrganizer(@PathVariable("id") Long id) {
 
-        Event output = eventService.getEvent(id);
+        Organizer output = organizerService.getOrganizer(id);
         if (output != null){
             return ResponseEntity.ok(output);
         }else {
@@ -36,8 +38,8 @@ public class OrganizerController {
     }
 
     @PostMapping("/organizers")
-    public ResponseEntity<?> addEvent(@RequestBody Event event) {
-        Event output = eventService.save(event);
-        return ResponseEntity.ok(event);
+    public ResponseEntity<?> addOrganizer(@RequestBody Organizer organizer) {
+        Organizer output = organizerService.save(organizer);
+        return ResponseEntity.ok(organizer);
     }
 }
