64b0df43894d1e00c7042b8a466b9914b295d358
Domdae <domdae@Phonmongkons-MacBook-Pro.local>
2021-09-02 20:14:51 +0700

    (3.4)chk pt2: error that beans with the same type

--- src/main/java/se331/lab/rest/entity/Event.java
+++ src/main/java/se331/lab/rest/entity/Event.java
@@ -1,11 +1,22 @@
 package se331.lab.rest.entity;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     Long id;
     String category;
     String title;
